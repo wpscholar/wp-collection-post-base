90ecbec561374b1d4f2ce3244fa45d076e70474b
Micah Wood <micah@wpscholar.com>
2019-02-06 16:50:55 -0500

    Updated post collection base to utilize collection base class. Removed dependency on PostsQuery class.

--- PostCollectionBase.php
+++ PostCollectionBase.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Abstract collection class for use with posts.
+ *
+ * @package wpscholar\WordPresss
+ */
 
 namespace wpscholar\WordPress;
 
@@ -7,111 +12,79 @@ namespace wpscholar\WordPress;
  *
  * @package wpscholar\WordPress
  */
-abstract class PostCollectionBase implements \Countable, \IteratorAggregate {
-
-	const POST_TYPE = null;
+abstract class PostCollectionBase extends CollectionBase {
 
 	/**
-	 * Contains WP_Query instance or pseudo WP_Query instance (stdClass object) if returned from cache
+	 * Post type name
 	 *
-	 * @var \WP_Query|\stdClass
+	 * @var string
 	 */
-	public $query;
+	const POST_TYPE = null;
 
 	/**
 	 * Default query args
 	 *
 	 * @var array
 	 */
-	protected $_default_args = [
+	protected $default_args = [
 		'ignore_sticky_posts' => true,
 	];
 
 	/**
-	 * @var \Traversable|\Countable
+	 * Internal storage of latest query.
+	 *
+	 * @var \WP_Query
 	 */
-	protected $_iterator;
+	protected $query;
 
 	/**
-	 * Collection constructor.
+	 * Fetch items
 	 *
-	 * @param array|string|null $args WP_Query arguments
-	 */
-	public function __construct( $args = null ) {
-		if ( ! is_null( $args ) ) {
-			$this->fetch( $args );
-		}
-	}
-
-	/**
-	 * Fetch posts
-	 *
-	 * @param array|string $args
+	 * @param array|string $args Query arguments
 	 */
 	public function fetch( $args = [] ) {
 
-		$required_args = [
-			'post_type' => static::POST_TYPE,
-		];
+		$args = wp_parse_args( $args );
 
-		$query_args = array_merge( $this->_default_args, wp_parse_args( $args ), $required_args );
+		$items = [];
 
-		$query = new PostsQuery();
-		$query->fetch( $query_args );
+		$query_args = array_merge(
+			$this->default_args,
+			$args,
+			array_merge(
+				[
+					'fields'    => 'ids',
+					'post_type' => static::POST_TYPE,
+				],
+				$this->required_args
+			)
+		);
 
-		$this->_iterator = $query;
-		$this->query     = $this->_iterator->query;
+		$this->query = new \WP_Query( $query_args );
 
-	}
-
-	/**
-	 * Check if this collection has posts
-	 *
-	 * @return bool
-	 */
-	public function havePosts() {
-		return $this->count() > 0;
-	}
-
-	/**
-	 * Count posts
-	 *
-	 * @return int
-	 */
-	public function count() {
-
-		// If iterator isn't set, just do a fetch automatically.
-		if ( ! isset( $this->_iterator ) ) {
-			$this->fetch();
+		if ( $this->query->have_posts() ) {
+			$items = $this->query->posts;
 		}
 
-		return count( $this->_iterator );
+		$this->populate( $items );
 	}
 
 	/**
-	 * Get iterator for collection
-	 *
-	 * @return \Generator
+	 * Get the found objects
 	 */
-	public function getIterator() {
-
-		// If iterator isn't set, just do a fetch automatically.
-		if ( ! isset( $this->_iterator ) ) {
-			$this->fetch();
-		}
-
-		foreach ( $this->_iterator as $post ) {
-			yield $this->_decorate( $post );
-		}
+	public function objects() {
+		return $this->collection()->map( 'get_post' );
 	}
 
 	/**
-	 * Decorate post
+	 * Transform ID into a post object.
 	 *
-	 * @param \WP_Post $post
+	 * @param int $id Post ID
 	 *
-	 * @return object
+	 * @return \WP_Post
 	 */
-	abstract protected function _decorate( \WP_Post $post );
+	protected function transform( $id ) {
+		return get_post( $id );
+	}
 
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+# WordPress Post Collection Base Class
+
+An abstract class that can be extended to create custom post collections in WordPress.
